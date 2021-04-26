c7360b23d6705cbf2857435445f39e961f65edb6
David Pal <paldavid2030@gmail.com>
2021-04-26 12:21:08 +0200

    Create connection to mongodb

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,19 @@ require('dotenv').config();
 
 const app = require('./app');
 const PORT = process.env.PORT || 4200;
+const mongoose = require('mongoose');
+console.log(process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+})
+  .then((_) => console.log('DB is connected'))
+  .catch((err) => {
+    // Do something when connection fails.
+    console.log(err.message);
+  });
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
