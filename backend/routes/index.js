3bd42b940211221767553f8c4d66d10db82685c8
David Pal <paldavid2030@gmail.com>
2021-04-26 15:00:46 +0200

    Create backend route: POST /update-post

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addPost } = require('../controllers');
+const { addPost, updatePost } = require('../controllers');
 const { catchAsyncErrors } = require('../middleware/errorHandlers');
 
 router.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-router.post('/add-post', catchAsyncErrors(addPost))
+router.post('/add-post', catchAsyncErrors(addPost));
+router.put('/update-post/:id', catchAsyncErrors(updatePost));
 
 module.exports = router;
