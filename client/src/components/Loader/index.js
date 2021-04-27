aa9fa5c12b372fc1204364aff7f1fa99449b2d72
David Pal <paldavid2030@gmail.com>
2021-04-27 17:30:20 +0300

    Adjust Backdrop component's z-index, include comment why

--- client/src/components/Loader/index.js
+++ client/src/components/Loader/index.js
@@ -3,8 +3,13 @@ import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 function Loader() {
+
+  /*
+  For some weird reason, the Backdrop component has a lower z-index than the Card component, here we apply a little inline quick fix. Since I couldn't find anything in the official documentation, I will just add an arbitrary number that is higher than the ones listed in the official repo:
+  https://github.com/mui-org/material-ui/blob/master/packages/material-ui/src/styles/zIndex.js
+  */
   return (
-    <Backdrop open>
+    <Backdrop open style={{ zIndex: 2000 }}>
       <CircularProgress color="primary" />
     </Backdrop>
   )
