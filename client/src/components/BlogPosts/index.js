1238b3095c540ee9b47fe6cd9dd3eee197192abe
David Pal <paldavid2030@gmail.com>
2021-04-27 17:35:14 +0300

    Display list of existing blog posts

--- client/src/components/BlogPosts/index.js
+++ client/src/components/BlogPosts/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BlogPost from './BlogPost';
 import Box from '@material-ui/core/Box';
+import { fetchPosts } from '../../redux/actions';
 
 function BlogPosts() {
+  const dispatch = useDispatch();
+  const posts = useSelector(store => store.posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
+
   return (
     <Box>
-      <BlogPost
-        title="Lorem ipsum"
-        text="When you walked through the door. It was clear to me (Clear to me). You’re the one they adore. Who they came to see (Who they came to see). You’re a rock star (Baby). Everybody wants you (Everybody wants you). Player, who can really blame you? (Who can really blame you?). We’re the ones who made you." />
+      {posts.map(post => (
+        <BlogPost
+          key={post._id}
+          id={post._id}
+          title={post.title}
+          text={post.text} />
+      ))}
     </Box>
   )
 }
