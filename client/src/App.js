b959a03a2e3b0e99802e196b28df7d1fbc7bc5d2
David Pal <paldavid2030@gmail.com>
2021-04-27 12:43:56 +0300

    Create Mui Theme and make font sizes responsive

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import Container from '@material-ui/core/Container'
 import AppHeader from './components/AppHeader';
 import BlogPosts from './components/BlogPosts';
+import ThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import responsiveFontSizes from '@material-ui/core/styles/responsiveFontSizes';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="md">
-        <AppHeader text="Awesome Blog App" />
-        <BlogPosts />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="md">
+          <AppHeader text="Awesome Blog App" />
+          <BlogPosts />
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
