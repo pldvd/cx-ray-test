8ed7378f056d16fd7a05b524e409c1d026812753
David Pal <paldavid2030@gmail.com>
2021-04-27 17:44:41 +0300

    Enable deleting blog posts

--- client/src/components/BlogPosts/BlogPost/index.js
+++ client/src/components/BlogPosts/BlogPost/index.js
@@ -7,8 +7,16 @@ import Typography from '@material-ui/core/Typography';
 import SaveIcon from '@material-ui/icons/Save';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { deletePost } from '../../../redux/actions';
+import { useDispatch } from 'react-redux';
 
-function BlogPost({ title, text }) {
+function BlogPost({ title, text, id }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deletePost(id));
+  };
+
   return (
     <Card>
       <CardContent>
@@ -23,6 +31,7 @@ function BlogPost({ title, text }) {
           Edit
         </Button>
         <Button
+          onClick={handleDelete}
           startIcon={<DeleteIcon />}
           variant="contained"
           color="secondary">
