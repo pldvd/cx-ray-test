11f9b96cfd48ff6d239595d3028bd4761f776eed
David Pal <paldavid2030@gmail.com>
2021-04-27 09:31:07 +0200

    Create backend route: GET /list-posts

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { addPost, updatePost, deletePost } = require('../controllers');
+const { addPost, updatePost, deletePost, listPosts } = require('../controllers');
 const { catchAsyncErrors } = require('../middleware/errorHandlers');
 const { idValidator } = require('../middleware/validators');
 
-router.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+router.get('/list-posts',
+  catchAsyncErrors(listPosts)
+);
 
 router.post('/add-post',
   catchAsyncErrors(addPost)
@@ -19,6 +19,7 @@ router.put('/update-post/:id',
 
 router.delete('/delete-post/:id',
   idValidator(),
-  catchAsyncErrors(deletePost));
+  catchAsyncErrors(deletePost)
+);
 
 module.exports = router;
