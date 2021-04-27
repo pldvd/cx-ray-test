5f1b996f8bf8fd914a4284b054dc54b52c97c049
David Pal <paldavid2030@gmail.com>
2021-04-27 11:26:39 +0200

    Update BlogPosts component: use MUI Box as wrapper instead plain div

--- client/src/components/BlogPosts/index.js
+++ client/src/components/BlogPosts/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import BlogPost from './BlogPost';
+import Box from '@material-ui/core/Box';
 
 function BlogPosts() {
   return (
-    <div>
-      <BlogPost title="Lorem ipsum" text="When you walked through the door. It was clear to me (Clear to me). You’re the one they adore. Who they came to see (Who they came to see). You’re a rock star (Baby). Everybody wants you (Everybody wants you). Player, who can really blame you? (Who can really blame you?). We’re the ones who made you."/>
-    </div>
+    <Box>
+      <BlogPost
+        title="Lorem ipsum"
+        text="When you walked through the door. It was clear to me (Clear to me). You’re the one they adore. Who they came to see (Who they came to see). You’re a rock star (Baby). Everybody wants you (Everybody wants you). Player, who can really blame you? (Who can really blame you?). We’re the ones who made you." />
+    </Box>
   )
 }
 
