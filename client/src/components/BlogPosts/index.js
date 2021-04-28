a833498563801319aa1f9b04e89d65bd29571d87
David Pal <paldavid2030@gmail.com>
2021-04-28 16:27:54 +0300

    Style BlogPosts component

--- client/src/components/BlogPosts/index.js
+++ client/src/components/BlogPosts/index.js
@@ -1,11 +1,21 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
 import BlogPost from './BlogPost';
 import Box from '@material-ui/core/Box';
 import { fetchPosts } from '../../redux/actions';
 
+const useStyles = makeStyles({
+  root: {
+    display: "grid",
+    gap: "2rem",
+    marginTop: "2rem"
+  }
+});
+
 function BlogPosts() {
   const dispatch = useDispatch();
+  const classes = useStyles();
   const posts = useSelector(store => store.posts);
 
   useEffect(() => {
@@ -13,7 +23,7 @@ function BlogPosts() {
   }, []);
 
   return (
-    <Box>
+    <Box className={classes.root}>
       {posts.map(post => (
         <BlogPost
           key={post._id}
