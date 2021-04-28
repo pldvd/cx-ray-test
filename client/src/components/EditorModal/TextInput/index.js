f49f787d23a9b7b5bf5184137e1f437bd1ec326b
David Pal <paldavid2030@gmail.com>
2021-04-28 14:01:34 +0300

    Show validation errors in TextInput and TitleInput components

--- client/src/components/EditorModal/TextInput/index.js
+++ client/src/components/EditorModal/TextInput/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import { ErrorMessage } from 'formik';
+import Box from '@material-ui/core/Box';
+import ValidationError from '../ValidationError';
 
 const useStyles = makeStyles({
   input: {
@@ -12,16 +15,26 @@ const useStyles = makeStyles({
 Field and form props are provided by Formik's Field component: https://formik.org/docs/api/field
 */
 
-function TextInput({field, form}) {
+function TextInput({ field, form }) {
   const classes = useStyles();
+  
+  const handleChange = e => {
+    form.setFieldTouched(field.name, true);
+    form.setFieldValue(field.name, e.target.value);
+  };
 
-  return <TextField
-    label="Text"
-    variant="outlined"
-    className={classes.input}
-    value={field.value}
-    onChange={e => form.setFieldValue(field.name, e.target.value)}
-  />;
+  return (
+    <Box>
+      <TextField
+        label="Text"
+        variant="outlined"
+        className={classes.input}
+        value={field.value}
+        onChange={handleChange}
+      />
+      <ErrorMessage name={field.name} component={ValidationError} />
+    </Box>
+  );
 }
 
 export default TextInput;
--- client/src/components/EditorModal/TitleInput/index.js
+++ client/src/components/EditorModal/TitleInput/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import ValidationError from '../ValidationError';
+import { ErrorMessage } from 'formik';
 
 const useStyles = makeStyles({
   input: {
@@ -15,13 +18,23 @@ Field and form props are provided by Formik's Field component: https://formik.or
 function TitleInput({ field, form }) {
   const classes = useStyles();
 
-  return <TextField
-    label="Title"
-    variant="standard"
-    className={classes.input}
-    value={field.value}
-    onChange={e => form.setFieldValue(field.name, e.target.value)}
-  />;
+  const handleChange = e => {
+    form.setFieldTouched(field.name, true);
+    form.setFieldValue(field.name, e.target.value);
+  };
+
+  return (
+    <Box>
+      <TextField
+        label="Title"
+        variant="standard"
+        className={classes.input}
+        value={field.value}
+        onChange={handleChange}
+      />
+      <ErrorMessage name={field.name} component={ValidationError} />
+    </Box>
+  );
 }
 
 export default TitleInput;
